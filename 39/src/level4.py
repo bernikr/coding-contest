cb74c48e11155e0b9b14705aaccd8b5de9f62843
bernikr <git@berni.kr>
2024-07-31 12:44:52 +0200

    add trial and error multiprocessing

--- 39/src/level4.py
+++ 39/src/level4.py
@@ -1,22 +1,16 @@
 from __future__ import annotations
 
-import filecmp
-import functools
 import operator
 import random
-import re
-import sys
 from collections import Counter, defaultdict
-from itertools import accumulate, count
-from pathlib import Path
+from itertools import accumulate
 from typing import List, Iterator
-
-from tqdm.contrib import tmap
-from tqdm.contrib.concurrent import thread_map
 
 
 class Vec(tuple[int, ...]):
-    def __new__(cls, *args: int | float) -> Vec:
+    def __new__(cls, *args: int | float):
+        if len(args) == 1:
+            return super().__new__(cls, *args)
         return super().__new__(cls, args)
 
     def __add__(self, other):
@@ -29,13 +23,13 @@ class Vec(tuple[int, ...]):
         if isinstance(other, int | float):
             return Vec(*map(lambda x: x * other, self))
         else:
-            raise NotImplemented
+            raise NotImplementedError
 
     def __truediv__(self, other):
         if isinstance(other, int | float):
             return Vec(*map(lambda x: x // other if x % other == 0 else x / other, self))
         else:
-            raise NotImplemented
+            raise NotImplementedError
 
     def manhatten(self):
         return sum(abs(x) for x in self)
@@ -60,12 +54,12 @@ def split_input(inp: List[str]) -> Iterator[tuple]:
         yield (mapp,)
 
 
-def solve(mapp) -> str:
+def solve(mapp) -> str | Iterator[str]:
     mapp = defaultdict(lambda: "#", mapp)
     path = ""
     poss = [[p for p, c in mapp.items() if c == "X"][0] + dirs[random.choice("WASD")]]  # maybe?
     d = random.choice("WASD")
-    for i in count():
+    for i in itertools.count():
         pos = poss[-1]
         opts = [x for x in "WASD" if mapp[pos + dirs[x]] == "." and pos + dirs[x] not in poss]
 
@@ -78,68 +72,111 @@ def solve(mapp) -> str:
                 path += d
                 poss.append(pos + dirs[d])
             else:
-                if validate_path(mapp, path):
-                    return path
+                yield path
+                if i < 1000 or i % 2 == 0:
+                    poss = [[p for p, c in mapp.items() if c == "X"][0] + dirs[random.choice("WASD")]]
                 else:
-                    if i < 1000 or i % 2 == 0:
-                        poss = [[p for p, c in mapp.items() if c == "X"][0] + dirs[random.choice("WASD")]]
-                    else:
-                        poss = [random.choice([p for p, c in mapp.items() if c == "."])]
-                    path = ""
-    return path
+                    poss = [random.choice([p for p, c in mapp.items() if c == "."])]
+                path = ""
 
 
-def validate_path(mapp, path):
-    poss = [Vec(0, 0)] + list(accumulate((dirs[l] for l in path), operator.add))
+def validate(path, mapp) -> bool:
+    poss = [Vec(0, 0)] + list(accumulate((dirs[x] for x in path), operator.add))
     mins = Vec(*map(min, zip(*poss)))
     poss = [p - mins for p in poss]
     return max(Counter(poss).values()) == 1 and all(c in poss for c, t in mapp.items() if t == ".") and all(
         mapp.get(p, "#") == "." for p in poss)
 
 
-#####################################
-WORKERS = 1
-RECOMPUTE = False
+##################################################
+WORKERS = 32
+RECOMPUTE = True
 CACHING = True
+##################################################
+
+import filecmp  # noqa: E402
+import functools  # noqa: E402
+import inspect  # noqa: E402
+import itertools  # noqa: E402
+import multiprocessing  # noqa: E402
+import re  # noqa: E402
+import sys  # noqa: E402
+from concurrent import futures  # noqa: E402
+from pathlib import Path  # noqa: E402
+
+from tqdm.contrib import tmap  # noqa: E402
+from tqdm.contrib.concurrent import process_map  # noqa: E402
+
+level = Path(__file__).name.split(".py")[0]
+leveldir = Path(__file__).parents[1] / "levels" / level
+partsdir = leveldir / "parts"
+is_trial_and_error = inspect.isgeneratorfunction(solve)
+
+
+def _solve_worker(inp, stop_event):
+    for candidate in solve(*inp):
+        if validate(candidate, *inp):
+            return candidate
+        if stop_event.is_set():
+            return None
+
+
+def _solve(inp, index):
+    if not is_trial_and_error:
+        res = solve(*inp)
+        if not validate(res, *inp):
+            print(f"⚠️ Failed to validate part {index}")
+        return res
+    if WORKERS == 1:
+        for candidate in solve(*inp):
+            if validate(candidate, *inp):
+                return candidate
+    else:
+        with multiprocessing.Manager() as manager:
+            stopping_event = manager.Event()
+            with futures.ProcessPoolExecutor(max_workers=WORKERS) as executor:
+                tasks = [executor.submit(_solve_worker, inp, stopping_event) for _ in range(WORKERS)]
+                futures.wait(tasks, return_when=futures.FIRST_COMPLETED)
+                res = next(futures.as_completed(tasks)).result()
+                stopping_event.set()
+        return res
+
+
+def _solve_cached(inp, index: int, outfile: Path):
+    if not CACHING:
+        return _solve(inp, index)
+    partfile = partsdir / (outfile.name + f".{index}")
+    if partfile.exists():
+        with partfile.open() as f:
+            return f.read()
+    else:
+        res = _solve(inp, index)
+        with partfile.open(mode="w") as f:
+            f.write(res)
+        return res
+
+
+def _run_file(infile, outfile):
+    with open(leveldir / infile) as f:
+        content = f.readlines()
+
+    print(f"{infile.name}")
+    preprocessed_input = list(split_input(content))
+
+    if WORKERS > 1 and not is_trial_and_error:
+        cmap = functools.partial(process_map, max_workers=WORKERS)
+    else:
+        cmap = tmap
+    cmap = functools.partial(cmap, miniters=1, file=sys.stdout, mininterval=0)
+    results = list(cmap(_solve_cached, preprocessed_input, itertools.count(), itertools.repeat(outfile)))
+
+    if results and len(results) > 0:
+        with open(leveldir / outfile, "w") as f:
+            f.write("\n".join(results))
+            f.write("\n")
 
 
 def _main():
-    level = Path(__file__).name.split(".py")[0]
-    leveldir = Path(__file__).parents[1] / "levels" / level
-    partsdir = leveldir / "parts"
-
-    def _solve(inp, index, outfile):
-        if not CACHING:
-            return solve(*inp)
-        partfile = partsdir / (outfile.name + f".{index}")
-        if partfile.exists():
-            with partfile.open() as f:
-                return f.read()
-        else:
-            res = solve(*inp)
-            with partfile.open(mode="w") as f:
-                f.write(res)
-            return res
-
-    def _run_file(infile, outfile):
-        with open(leveldir / infile) as f:
-            content = f.readlines()
-
-        print(f"{infile.name}")
-        preprocessed_input = list(split_input(content))
-
-        if WORKERS > 1:
-            cmap = functools.partial(thread_map, max_workers=WORKERS)
-        else:
-            cmap = tmap
-        cmap = functools.partial(cmap, miniters=1, file=sys.stdout, mininterval=0)
-        results = list(cmap(functools.partial(_solve, outfile=outfile), preprocessed_input, count()))
-
-        if results and len(results) > 0:
-            with open(leveldir / outfile, "w") as f:
-                f.write("\n".join(results))
-                f.write("\n")
-
     if CACHING:
         partsdir.mkdir(exist_ok=True)
         if RECOMPUTE:
@@ -151,11 +188,14 @@ def _main():
             file.unlink()
 
     example_in_file = leveldir / (level + "_example.in")
-    _run_file(example_in_file, example_in_file.with_suffix(".out.computed"))
-    if filecmp.cmp(example_in_file.with_suffix(".out"), example_in_file.with_suffix(".out.computed")):
-        print("✅ Example check passed")
+    if not example_in_file.exists():
+        print("⚠️ No example file found")
     else:
-        print("⚠️ Example check failed")
+        _run_file(example_in_file, example_in_file.with_suffix(".out.computed"))
+        if filecmp.cmp(example_in_file.with_suffix(".out"), example_in_file.with_suffix(".out.computed")):
+            print("✅ Example check passed")
+        else:
+            print("⚠️ Example check failed")
 
     for file in leveldir.iterdir():
         if re.match(r"level\d+_\d+\.in", file.name):
--- _template/src/level6.py
+++ _template/src/level6.py
@@ -1,71 +1,112 @@
 from __future__ import annotations
 
-import filecmp
 import re
-import sys
-from functools import partial
-from itertools import count
-from pathlib import Path
+from itertools import count, repeat
 from typing import List, Iterator
-
-from tqdm.contrib import tmap
-from tqdm.contrib.concurrent import thread_map
 
 
 ##################################################
 
 def split_input(inp: List[str]) -> Iterator[tuple]:
     for line in inp:
-        yield (line,)  # output tuple will be input params of solve method
+        yield (line,)  # output tuple will be input params of solve and validate method
 
 
 def solve(line) -> str:
     return str(len(line))
 
 
+def validate(solution: str, line) -> bool:
+    return True
+
+
 ##################################################
 WORKERS = 1
-RECOMPUTE = True
+RECOMPUTE = False
 CACHING = True
+##################################################
+
+import filecmp  # noqa: E402
+import functools  # noqa: E402
+import inspect  # noqa: E402
+import multiprocessing  # noqa: E402
+import sys  # noqa: E402
+from concurrent import futures  # noqa: E402
+from pathlib import Path  # noqa: E402
+
+from tqdm.contrib import tmap  # noqa: E402
+from tqdm.contrib.concurrent import process_map  # noqa: E402
+
+level = Path(__file__).name.split(".py")[0]
+leveldir = Path(__file__).parents[1] / "levels" / level
+partsdir = leveldir / "parts"
+is_trial_and_error = inspect.isgeneratorfunction(solve)
+
+
+def _solve_worker(inp, stop_event):
+    for candidate in solve(*inp):
+        if validate(candidate, *inp):
+            return candidate
+        if stop_event.is_set():
+            return None
+
+
+def _solve(inp, index):
+    if not is_trial_and_error:
+        res = solve(*inp)
+        if not validate(res, *inp):
+            print(f"⚠️ Failed to validate part {index}")
+        return res
+    if WORKERS == 1:
+        for candidate in solve(*inp):
+            if validate(candidate, *inp):
+                return candidate
+    else:
+        with multiprocessing.Manager() as manager:
+            stopping_event = manager.Event()
+            with futures.ProcessPoolExecutor(max_workers=WORKERS) as executor:
+                tasks = [executor.submit(_solve_worker, inp, stopping_event) for _ in range(WORKERS)]
+                futures.wait(tasks, return_when=futures.FIRST_COMPLETED)
+                res = next(futures.as_completed(tasks)).result()
+                stopping_event.set()
+        return res
+
+
+def _solve_cached(inp, index: int, outfile: Path):
+    if not CACHING:
+        return _solve(inp, index)
+    partfile = partsdir / (outfile.name + f".{index}")
+    if partfile.exists():
+        with partfile.open() as f:
+            return f.read()
+    else:
+        res = _solve(inp, index)
+        with partfile.open(mode="w") as f:
+            f.write(res)
+        return res
+
+
+def _run_file(infile, outfile):
+    with open(leveldir / infile) as f:
+        content = f.readlines()
+
+    print(f"{infile.name}")
+    preprocessed_input = list(split_input(content))
+
+    if WORKERS > 1 and not is_trial_and_error:
+        cmap = functools.partial(process_map, max_workers=WORKERS)
+    else:
+        cmap = tmap
+    cmap = functools.partial(cmap, miniters=1, file=sys.stdout, mininterval=0)
+    results = list(cmap(_solve_cached, preprocessed_input, count(), repeat(outfile)))
+
+    if results and len(results) > 0:
+        with open(leveldir / outfile, "w") as f:
+            f.write("\n".join(results))
+            f.write("\n")
 
 
 def _main():
-    level = Path(__file__).name.split(".py")[0]
-    leveldir = Path(__file__).parents[1] / "levels" / level
-    partsdir = leveldir / "parts"
-
-    def _solve(inp, index, outfile):
-        if not CACHING:
-            return solve(*inp)
-        partfile = partsdir / (outfile.name + f".{index}")
-        if partfile.exists():
-            with partfile.open() as f:
-                return f.read()
-        else:
-            res = solve(*inp)
-            with partfile.open(mode="w") as f:
-                f.write(res)
-            return res
-
-    def _run_file(infile, outfile):
-        with open(leveldir / infile) as f:
-            content = f.readlines()
-
-        print(f"{infile.name}")
-        preprocessed_input = list(split_input(content))
-
-        if WORKERS > 1:
-            cmap = partial(thread_map, max_workers=WORKERS)
-        else:
-            cmap = tmap
-        cmap = partial(cmap, miniters=1, file=sys.stdout, mininterval=0)
-        results = list(cmap(partial(_solve, outfile=outfile), preprocessed_input, count()))
-
-        if results and len(results) > 0:
-            with open(leveldir / outfile, "w") as f:
-                f.write("\n".join(results))
-                f.write("\n")
-
     if CACHING:
         partsdir.mkdir(exist_ok=True)
         if RECOMPUTE:
